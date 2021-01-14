0915896e45f149e9ed82d5ed058fc7fd41a51f51
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-01-14 14:39:15 -0600

    jogging my memory about how the js drag and drop api works so I can relate it to my towers of hanoi project

---
+++ script.js
@@ -0,0 +1,18 @@
+function onDragStart(ev) {
+    ev.dataTransfer.setData('text/plain', ev.target.id);
+    ev.currentTarget.style.backgroundColor = 'yellow';
+}
+
+function onDragOver(ev) {
+    ev.preventDefault();
+}
+
+function onDrop(ev) {
+    const id = ev.dataTransfer.getData('text');
+    const draggableElement = document.getElementById(id);
+    const dropzone = ev.target;
+
+    dropzone.appendChild(draggableElement);
+
+    ev.dataTransfer.clearData();
+}
